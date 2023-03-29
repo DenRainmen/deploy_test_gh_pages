bb639481ef93e6ada04474f92acebf4fa073bd46
Den <potarykindenis@gmail.com>
2023-03-30 01:52:36 +0300

    Change the subtittle

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Измените <code>src/App.tsx</code> и сохраните изменения чтобы они вступили в силу.
         </p>
       </div>
       <p className="read-the-docs">
